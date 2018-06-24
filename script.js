6cdb3a5b118eca8e50db4718e35f968fb564b724
robin <robin.rb.luo@gmail.com>
2018-06-24 16:57:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var timer;
 /**
  * Gets first batch of tracks if there are any.
  **/
-$.getScript(username + ".tumblr.com/api/read/json?type=audio", function() {
+$.getScript("//" + username + ".tumblr.com/api/read/json?type=audio", function() {
     postsTotal = tumblr_api_read["posts-total"];
     postsStart = tumblr_api_read["posts-start"];
     console.log(postsTotal + " is the total amount of audio posts on " + username + ".tumblr.com.");
